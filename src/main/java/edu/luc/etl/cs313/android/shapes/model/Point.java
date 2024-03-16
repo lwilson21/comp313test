1dbf15c5975f3c77fb20c6ae095590ae718be5c6
lwilson21 <123020389+lwilson21@users.noreply.github.com>
2024-03-15 23:34:13 -0500

    Add files via upload

---
+++ src/main/java/edu/luc/etl/cs313/android/shapes/model/Point.java
@@ -0,0 +1,13 @@
+package edu.luc.etl.cs313.android.shapes.model;
+
+/**
+ * A point, implemented as a location without a shape.
+ */
+public class Point extends Location {
+
+	public Point(final int x, final int y) {
+		super(x, y, new Circle(0));
+		assert x >= 0;
+		assert y >= 0;
+	}
+}
